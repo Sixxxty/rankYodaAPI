78ec14d351df68b46643c981ed6e7a9c0dc8c5bf
Sixxxty <48228517+Sixxxty@users.noreply.github.com>
2020-05-27 20:22:49 -0300

    Update rankYoda.js

--- rankYoda.js
+++ rankYoda.js
@@ -4,27 +4,6 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('/',(req, res) =>{
-	let ranks = [];
-request('https://shadowarena.pearlabyss.com/en-US/Arena?battleType=0&server=sa',function( err, res, body){
-	if(err) console.log('Erro: ' + err);
-	var $ = cheerio.load(body);
-	$('.box_list_area').each(function(){
-		var name = $(this).find('.thum_name').text()
-		ranks.push(name)
-
-	});
-	for(var i = 0; i <ranks.length; i++){
-		if(ranks[i] === "YoDaSL"){
-			console.log("YoDaSL está atualmente top:", i+1,"do servidor SA")
-			break
-		}
-	}
-
-});
-
-})
-
 let ranks = [];
 request('https://shadowarena.pearlabyss.com/en-US/Arena?battleType=0&server=sa',function( err, res, body){
 	if(err) console.log('Erro: ' + err);
@@ -36,9 +15,19 @@ request('https://shadowarena.pearlabyss.com/en-US/Arena?battleType=0&server=sa',
 	});
 	for(var i = 0; i <ranks.length; i++){
 		if(ranks[i] === "YoDaSL"){
-			console.log("YoDaSL está atualmente top:", i+1,"do servidor SA")
+			let rankYo = i+1
+			app.get('/', (req, res) => {
+				res.json({
+					msg: "YoDa está top", rankYo
+				})
+			})
+			
 			break
 		}
 	}
 
 });
+
+
+
+

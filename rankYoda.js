31490040243821dcb53a83fdfaeb2ce49854d511
Sixxxty <48228517+Sixxxty@users.noreply.github.com>
2020-05-27 21:45:55 -0300

    Update rankYoda.js

--- rankYoda.js
+++ rankYoda.js
@@ -20,7 +20,7 @@ request('https://shadowarena.pearlabyss.com/en-US/Arena?battleType=0&server=sa',
 				console.log("Escutando")
 			})
 			app.get('/', (req, res) => {
-				res.send("Top 7")
+				res.send(`Top: ${rankYo}`)
 					
 				
 					
